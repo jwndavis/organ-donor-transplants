b24fea96d9ecdd86d8e3af30a1e36f29061347c6
John W N Davis <jwda232@uky.edu>
2017-03-26 00:07:04 -0400

    Project 09

--- js/app.js
+++ js/app.js
@@ -42,18 +42,24 @@
         "INTESTINE": "Intestine"
     }
 
-    var attributeValue = "ALL_ORG",
-        normValue = "ALL_DON";
+//    var attributeValue = "ALL_ORG",
+//        normValue = "ALL_DON";
 
     var currentYear = "1991",
-        currentType = "ALL";
+        currentTransplantType = "ALL",
+        currentDonorType = "ALL"
+    
+    
+    var transplantLayer,
+        donorLayer;
+    
     
     function drawMap(data) {
 
         console.log(data);
 
-        var transplantLayer = L.geoJson(data, options).addTo(map);
-        var donorLayer = L.geoJson(data, options).addTo(map);
+        transplantLayer = L.geoJson(data, options).addTo(map);
+        donorLayer = L.geoJson(data, options).addTo(map);
 
         //     map.fitBounds(transplantLayer.getBounds());
 
@@ -63,15 +69,17 @@
         donorLayer.setStyle({
             color: 'blue',
         });
+
+        resizeCircles();
+
+        sequenceUI();
+
+        addUitransplants();
+
+        addUidonors();
+
+        retrieveInfo();
         
-        resizeCircles(transplantLayer, donorLayer, currentYear, currentType);
-
-        sequenceUI(transplantLayer, donorLayer);
-
-        addUitransplants(transplantLayer);
-
-        addUidonors(donorLayer);
-
         //updateMap(transplantLayer, donorLayer);
         
     }
@@ -99,22 +107,24 @@
         return radius * 1.5;
     }
 
-    function resizeCircles(transplantLayer, donorLayer, currentYear, currentType) {
+    function resizeCircles() {
 
         transplantLayer.eachLayer(function (layer) {
-            var radius = calcRadius(Number(layer.feature.properties['T' + currentYear + '_' + currentType]));
+            var radius = calcRadius(Number(layer.feature.properties['T' + currentYear + '_' + currentTransplantType]));
+            if(Number(radius)) {
             layer.setRadius(radius);
+            }
         });
         donorLayer.eachLayer(function (layer) {
-            var radius = calcRadius(Number(layer.feature.properties['D' + currentYear + '_' + currentType]));
+            var radius = calcRadius(Number(layer.feature.properties['D' + currentYear + '_' + currentDonorType]));
+            if(Number(radius)) {
             layer.setRadius(radius);
+            }
         });
-
-        retrieveInfo(transplantLayer, donorLayer, currentYear);
 
     }
 
-    function sequenceUI(transplantLayer, donorLayer) {
+    function sequenceUI() {
 
         var sliderControl = L.control({
             position: 'bottomleft'
@@ -153,15 +163,15 @@
 
         $('.slider')
             .on('input change', function () {
-                var currentYear = $(this).val();
-                resizeCircles(transplantLayer, donorLayer, currentYear, currentType);
+                currentYear = $(this).val();
+                resizeCircles();
 
                 output.html(currentYear);
             });
 
     }
 
-    function addUitransplants(transplantLayer) {
+    function addUitransplants() {
 
         var transplantMenu = L.control({
             position: 'topright'
@@ -180,9 +190,9 @@
 
         $('select[id="ALL_ORG"]').change(function () {
 
-            attributeValue = $(this).val();
+            currentTransplantType = $(this).val();
 
-            //updateMap(transplantLayer);
+            resizeCircles();
         });
 
     }
@@ -204,11 +214,11 @@
         }
         donorMenu.addTo(map);
 
-        $('select[id="ALL_DON"]').change(function () {
+        $('select[id="ALL_DON"]').change(function () {  
 
-            normValue = $(this).val();
+            currentType = $(this).val();
 
-            //updateMap(donorLayer);
+            resizeCircles();
         });
 
     }
@@ -243,7 +253,7 @@
                     dataValues.push(attribute);
                 }
             }
-            //   console.log(dataValues);
+            
         });
 
         var sortedValues = dataValues.sort(function (a, b) {
@@ -254,7 +264,6 @@
 
         var maxValues = Math.round(sortedValues[0] / 1000) * 1000;
 
-        console.log(maxValues);
 
         var largeDiameter = calcRadius(maxValues) * 2,
             smallDiameter = largeDiameter / 2;
@@ -291,7 +300,7 @@
 
     }
 
-    function retrieveInfo(transplantLayer, donorLayer, currentYear, currentType) {
+    function retrieveInfo() {
 
         var info = $('#info');
 
